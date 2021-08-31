9d8c7dc2b0fb1f492f25c6b1c8eabfb1a72d7ecb
natsnet <natsnetf90@gmail.com>
2021-08-31 06:22:55 -0500

    comit

--- assignment2.js
+++ assignment2.js
@@ -1,21 +1,21 @@
 let promote=require('prompt-sync')();
-// let age=promote("enter your age");
+let age=promote("enter your age");
 
-//  while(age < 18){
-//     console.log("your age is invalid " + age);
-//     age=promote("enter your age");
-//  }
-//   console.log(" valid age " + age);
+ while(age < 18){
+    console.log("your age is invalid " + age);
+    age=promote("enter your age");
+ }
+  console.log(" valid age " + age);
 
 
  
  
-//let age;
+//let age1;
 do{
-   let age=promote("please enter your age");
-    console.log("your age is " + age);
+   let age1=promote("please enter your age");
+    console.log("your age is " + age1);
 }
-while(age < 18);
+while(age1 < 18);
 console.log("stop")
 
 
