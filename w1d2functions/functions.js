c40adbf3569f85f96c5645dbe2b82e88063b1c82
NATSNETF <84424521+NATSNETF@users.noreply.github.com>
2021-09-01 16:30:34 -0500

    Update functions.js

--- w1d2functions/functions.js
+++ w1d2functions/functions.js
@@ -1,1 +1,19 @@
-console.log("test");
+function SalesCommission(x,sale){
+    if(x === false){
+        if( sale >= 300 && sale <= 500){
+            console.log( sale * 0.02);
+        }else(sale > 500)
+        console.log(sale * 0.03);
+    }else 
+    if(x=== true){
+        console.log("no commission");
+    }else if(sale >= 300 && sale <= 500){
+        console.log("yir commission is " + sale * 0.01);
+    }else if(sale > 500){
+        console.log("your salary is " + (sale * 0.02));
+    }
+
+ }
+ SalesCommission(true, 200);
+
+
